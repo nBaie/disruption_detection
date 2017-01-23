441f6c9d88228e0a0da6805fa6002fdc70556a35
nBaie <niklas.baie@stud.uni-goettingen.de>
2017-01-23 17:05:52 +0100

    sql

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -1,6 +1,13 @@
+create SCHEMA disruption_detection;
+commit;
+
 create table Documents (
-    idDocuments INT,
-    vector blob,
-    text mediumtext,
+    idDocuments INT NOT NULL AUTO_INCREMENT,
+    vector blob NOT NULL,
+    text mediumtext NOT NULL,
     decision_value float,
     url mediumtext);
+
+--keine Text-Duplikate einfügen
+ALTER IGNORE TABLE `Documents`
+ADD UNIQUE INDEX (`text`);
